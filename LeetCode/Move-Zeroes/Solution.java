1ca04db47472d6799d82ee4aa455b7d837bb3ac3
Aditya-12-k <adityakannojiya2512@gmail.com>
2026-08-12 12:27:54 +0530

    Add solution for Move Zeroes

---
+++ LeetCode/Move-Zeroes/Solution.java
@@ -0,0 +1,13 @@
+1class Solution {
+2    public void moveZeroes(int[] nums) {
+3        int slow = 0;
+4        for(int fast=0; fast<nums.length; fast++){
+5            if(nums[fast]!=0){
+6                int temp = nums[slow];
+7                nums[slow] = nums[fast];
+8                nums[fast] = temp;
+9                slow++;
+10            }
+11        }
+12    }
+13}
